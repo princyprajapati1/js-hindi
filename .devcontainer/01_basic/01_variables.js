08e350fe11c565e83b59891b013c515ed94eb847
Princy <princy.prajapati81@gmail.com>
2026-06-03 16:52:15 +0530

    datatypes

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 14456
+let accountEmail = "princy@google.com"
+var accountPass = "12345"
+accountCity = "Ahmedabad"
+
+// accountID = 2 not allowed
+
+accountEmail = "hc.hc@gamil.com"
+accountPass = "123" 
+accountCity = "Mahesanas"
+console.table([accountEmail, accountPass, accountCity]);
+
